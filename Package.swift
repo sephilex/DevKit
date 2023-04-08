8a0411d95b1a392975a18daa195ac8ba9429cf1b
Sephilex <sephilex@gmail.com>
2023-04-08 19:32:18 +0800

    sephilex--2023-04-08-19:32:18

--- Package.swift
+++ Package.swift
@@ -7,25 +7,25 @@ let package = Package(
     products: [
         .library(name: "Core", targets: ["Core"])
     ],
+  //   dependencies: [
+  //   .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
+  // ],
+  //   targets: [
+  //   .target(name: "Core", dependencies: ["RxSwift", "RxCocoa"])
+  // ]
     dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
-  ],
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+        // .package(url: "https://github.com/RxSwiftCommunity/RxSwiftExt.git", .upToNextMajor(from: "6.1.0")),
+        .package(url: "https://github.com/RxSwiftCommunity/NSObject-Rx.git", .upToNextMajor(from: "5.0.0")),
+        // .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0")),
+        .package(url: "https://github.com/roberthein/TinyConstraints.git", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/devxoul/Then.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/SwifterSwift/SwifterSwift.git", .upToNextMajor(from: "5.0.0"))
+    ],
     targets: [
-    .target(name: "Core", dependencies: ["RxSwift", "RxCocoa"])
-  ]
-    // dependencies: [
-    //     .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-    //     // .package(url: "https://github.com/RxSwiftCommunity/RxSwiftExt.git", .upToNextMajor(from: "6.1.0")),
-    //     .package(url: "https://github.com/RxSwiftCommunity/NSObject-Rx.git", .upToNextMajor(from: "5.0.0")),
-    //     // .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.0")),
-    //     .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0")),
-    //     .package(url: "https://github.com/roberthein/TinyConstraints.git", .upToNextMajor(from: "4.0.0")),
-    //     .package(url: "https://github.com/devxoul/Then.git", .upToNextMajor(from: "3.0.0")),
-    //     .package(url: "https://github.com/SwifterSwift/SwifterSwift.git", .upToNextMajor(from: "5.0.0"))
-    // ],
-    // targets: [
-    //     .target(
-    //         name: "Core", 
-    //         dependencies: ["RxSwift", "RxCocoa", "NSObject-Rx", "Kingfisher", "TinyConstraints", "Then", "SwifterSwift"])
-    // ]
+        .target(
+            name: "Core", 
+            dependencies: ["RxSwift", "RxCocoa", "NSObject-Rx", "Kingfisher", "TinyConstraints", "Then", "SwifterSwift"])
+    ]
 )
