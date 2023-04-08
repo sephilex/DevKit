79cc7abc5b09f5153f49b2eecd600f93a90d8070
Sephilex <sephilex@gmail.com>
2023-04-08 19:31:37 +0800

    sephilex--2023-04-08-19:31:37

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
     .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
   ],
-  targets: [
+    targets: [
     .target(name: "Core", dependencies: ["RxSwift", "RxCocoa"])
   ]
     // dependencies: [
